ecac3b79de2fcafe99a080e2fbd65294f663525e
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2022-01-20 17:04:19 +0300

    Update join.cpp

--- join.cpp
+++ join.cpp
@@ -2,19 +2,17 @@
 
 string itc_join(vector <char> lst, string sep)
 {
-	string out = "";
-	int i = 0, len = lst.size();
-	while (i < len)
-	{
-		if (len - i != 1)
-		{
-			out = out + lst[i] + sep;
-		}
-		else
-		{
-			out = out + lst[i];
-		}
-		i++;
+	string n = "";
+	int i;
+	i = 0;
+	int len;
+	len = lst.size();
+	while (i < len){
+            if (len - i != 1)
+		n = n + lst[i] + sep;
+	    else
+		n = n + lst[i];
+	i = i + 1;
 	}
-	return out;
+	return n;
 }
