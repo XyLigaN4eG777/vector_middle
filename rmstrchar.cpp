460fc99fc6e66a12e5517d88741b5ee27ed3e0a5
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2022-01-20 19:27:50 +0300

    Update rmstrchar.cpp

--- rmstrchar.cpp
+++ rmstrchar.cpp
@@ -2,26 +2,29 @@
 
 string itc_rmstrchar(string str, string less)
 {
-	int len1 = get_len_str(str), len2 = get_len_str(less), i = 0, s = 0;
-	string out = "";
+	int len1;
+	len1 = get_len_str(str);
+	int len2;
+	len2 = get_len_str(less);
+	int i;
+	i= 0;
+	int s;
+	s = 0;
+	string n = "";
 	bool isDel = 0;
 	while (i < len1)
 	{
-		while (s < len2)
+	    while (s < len2)
 		{
-			if (str[i] == less[s])
-			{
-				isDel = 1;
-			}
-			s++;
+		if (str[i] == less[s])
+			isDel = 1;
+		s = s + 1;
 		}
-		s = 0;
-		if (isDel == 0)
-		{
-			out = out + str[i];
-		}
-		isDel = 0;
-		i++;
+	    s = 0;
+	    if (isDel == 0)
+		n = n + str[i];
+	    isDel = 0;
+	    i = i + 1;
 	}
-	return out;
+	return n;
 }
