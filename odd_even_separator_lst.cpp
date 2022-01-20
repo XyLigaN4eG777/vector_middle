97f8b529b4c7d39a6a05315cc39d5248d2506155
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2022-01-20 17:11:33 +0300

    Update odd_even_separator_lst.cpp

--- odd_even_separator_lst.cpp
+++ odd_even_separator_lst.cpp
@@ -2,18 +2,17 @@
 
 void itc_odd_even_separator_lst(const vector <int>& lst, vector <int>& lst1, vector <int>& lst2)
 {
-	int i = 0, len = lst.size();
+	int i;
+	i = 0;
+	int len;
+	len = lst.size();
 	while (i < len)
 	{
-		if (lst[i] % 2 == 0)
-		{
-			lst1.push_back(lst[i]);
-		}
-		else
-		{
-			lst2.push_back(lst[i]);
-		}
-		i++;
+	    if (lst[i] % 2 == 0)
+		lst1.push_back(lst[i]);
+	    else
+		lst2.push_back(lst[i]);
+	    i = i + 1;
 	}
 	out_vect_int(lst1);
 	cout << endl;
